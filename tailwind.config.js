7d39dfaa024aa10e7c0f185b84c4dbe1c929847d
Koksi-cyber <godswillekong4@gmail.com>
2023-04-14 23:14:51 +0100

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge :[
-    "*.html",
-    '*js'
-  ], 
+  purge: ["*.html", "*js"],
   content: ["*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ],
-}
+  plugins: [require("tailwindcss"), require("autoprefixer")],
+  theme: {
+    extend: {
+      fontFamily: {
+        "mergic": ["Mergic", "sans-serif"],
+        "sigmar-one": ["SigmarOne", "sans-serif"],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
